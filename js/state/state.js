bb82b73e4585d5db7d0ba34cd37f6c2836e4c1d8
Shiva shankar <shivashankar@Shivas-MacBook-Air.local>
2022-02-16 09:05:34 +0530

    data added

--- js/state/state.js
+++ js/state/state.js
@@ -22,3 +22,20 @@ export const blockElements = [
 	"dc limited circuit",
 	"parameter extraction",
 ];
+
+const tutorialData = {
+	NBFM: [
+		{ AM: 5, FM: 750, AC: 20, FC: 1000 },
+		{ AM: 10, FM: 500, AC: 15, FC: 1000 },
+		{ AM: 10, FM: 1500, AC: 15, FC: 2000 },
+		{ AM: 12, FM: 1000, AC: 10, FC: 2500 },
+		{ AM: 15, FM: 2000, AC: 12, FC: 5000 },
+	],
+	WBFM: [
+		{ AM: 5, FM: 500, AC: 10, FC: 1000 },
+		{ AM: 8, FM: 250, AC: 15, FC: 750 },
+		{ AM: 10, FM: 500, AC: 15, FC: 1500 },
+		{ AM: 10, FM: 750, AC: 15, FC: 2000 },
+		{ AM: 15, FM: 750, AC: 20, FC: 5000 },
+	],
+};
